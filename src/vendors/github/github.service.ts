0cb92d5ce4044fc643bf6478d02d62cacfa1f2f9
barzamsky <barzamsky1@gmail.com>
2022-10-30 16:20:49 +0200

    files

--- src/vendors/github/github.service.ts
+++ src/vendors/github/github.service.ts
@@ -1,6 +1,11 @@
+import { HttpService } from "@nestjs/axios";
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class GithubService {
-  
+  constructor(private readonly httpService: HttpService) {}
+
+  getRepoFilesTree() {
+    console.log('hello')
+  }
 }
